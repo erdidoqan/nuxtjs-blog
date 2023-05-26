ddfc3e79e0684eed7277d3a4175345ed8f327de0
erdi <erdi@digitexa.com>
2023-05-26 14:02:28 +0300

    google verification

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -2,72 +2,33 @@
   <footer>
     <div class="px-4 py-24 mx-auto overflow-hidden mt-20 sm:px-6 lg:px-8 bg-gray-100 rounded-t-xl">
       <nav class="flex flex-wrap justify-center -mx-5 -my-2" aria-label="Footer">
-        <div class="px-5 py-2">
-          <a href="https://quizgecko.com/quizzes" class="text-base hover:text-gray-400">
-            Quiz Examples
+
+        <div
+          v-for="(footertag, key) in users"
+          :key="key"
+          class="px-5 py-2"
+        >
+          <a href="/quizzes" class="text-base hover:text-gray-400">
+            {{ footertag.title }}
           </a>
         </div>
-        <div class="px-5 py-2">
-          <a href="https://quizgecko.com/library" class="text-base hover:text-gray-400">
-            My Library
-          </a>
-        </div>
-        <div class="px-5 py-2">
-          <a href="https://quizgecko.com/api " target="_blank" class="text-base hover:text-gray-400">
-            API
-          </a>
-        </div>
-        <div class="px-5 py-2">
-          <a href="https://quizgecko.com/generators" class="text-base hover:text-gray-400">
-            Quiz Generators
-          </a>
-        </div>
-        <div class="px-5 py-2">
-          <a href="https://chrome.google.com/webstore/detail/quizgecko/jipdeflholipfmimeegaphokjfhkbbpl" target="_blank" class="text-base hover:text-gray-400">
-            Chrome Extension
-            <svg class="inline-block w-4 h-4" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" viewBox="0 0 24 24" fill="currentColor"><rect fill="none" height="24" width="24" x="0" y="0"></rect><path d="M12,4c4.41,0,8,3.59,8,8s-3.59,8-8,8s-8-3.59-8-8S7.59,4,12,4 M12,2C6.48,2,2,6.48,2,12c0,5.52,4.48,10,10,10s10-4.48,10-10 C22,6.48,17.52,2,12,2L12,2z M13.88,11.54l-4.96,4.96l-1.41-1.41l4.96-4.96L10.34,8l5.65,0.01L16,13.66L13.88,11.54z"></path></svg> </a>
-        </div>
-        <div class="px-5 py-2">
-          <a href="https://quizgecko.getrewardful.com/signup" target="_blank" class="text-base hover:text-gray-400">
-            Affiliates
-            <svg class="inline-block w-4 h-4" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" viewBox="0 0 24 24" fill="currentColor"><rect fill="none" height="24" width="24" x="0" y="0"></rect><path d="M12,4c4.41,0,8,3.59,8,8s-3.59,8-8,8s-8-3.59-8-8S7.59,4,12,4 M12,2C6.48,2,2,6.48,2,12c0,5.52,4.48,10,10,10s10-4.48,10-10 C22,6.48,17.52,2,12,2L12,2z M13.88,11.54l-4.96,4.96l-1.41-1.41l4.96-4.96L10.34,8l5.65,0.01L16,13.66L13.88,11.54z"></path></svg> </a>
-        </div>
-        <div class="px-5 py-2">
-          <a href="https://quizgecko.canny.io/feature-requests" target="_blank" class="text-base hover:text-gray-400">
-            Feature Requests
-            <svg class="inline-block w-4 h-4" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" viewBox="0 0 24 24" fill="currentColor"><rect fill="none" height="24" width="24" x="0" y="0"></rect><path d="M12,4c4.41,0,8,3.59,8,8s-3.59,8-8,8s-8-3.59-8-8S7.59,4,12,4 M12,2C6.48,2,2,6.48,2,12c0,5.52,4.48,10,10,10s10-4.48,10-10 C22,6.48,17.52,2,12,2L12,2z M13.88,11.54l-4.96,4.96l-1.41-1.41l4.96-4.96L10.34,8l5.65,0.01L16,13.66L13.88,11.54z"></path></svg> </a>
-        </div>
-        <div class="px-5 py-2">
-          <a href="https://quizgecko.com/legal" class="text-base hover:text-gray-400">
-            Legal
-          </a>
-        </div>
+
       </nav>
       <div class="flex justify-center mt-6 space-x-6">
-        <a target="_blank" href="https://twitter.com/Quizgecko" class="text-gray-400 cursor-pointer hover:text-gray-700">
+        <a target="_blank" href="https://twitter.com/GreetingBirds" class="text-gray-400 cursor-pointer hover:text-gray-700">
           <span class="sr-only">Twitter</span>
-          <svg class="w-6 h-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z">
-            </path>
-          </svg>
+          <svg class="w-6 h-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>
         </a>
-        <a target="_blank" href="mailto:hello@quizgecko.com" class="text-gray-400 cursor-pointer hover:text-gray-700">
+        <a target="_blank" href="mailto:greetingbirdscom@gmail.com" class="text-gray-400 cursor-pointer hover:text-gray-700">
           <span class="sr-only">Email</span>
           <svg class="inline-block w-6 h-7 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M0 0h24v24H0V0z" fill="none"></path><path d="M22 6c0-1.1-.9-2-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6zm-2 0l-8 4.99L4 6h16zm0 12H4V8l8 5 8-5v10z"></path></svg> </a>
-        <a target="_blank" href="https://chrome.google.com/webstore/detail/quizgecko/jipdeflholipfmimeegaphokjfhkbbpl" class="text-gray-400 cursor-pointer hover:text-gray-700">
+        <a target="_blank" href="GreetingBirds" class="text-gray-400 cursor-pointer hover:text-gray-700">
           <span class="sr-only">Chrome</span>
-          <svg class="w-6 h-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-            <circle cx="12" cy="12" r="10"></circle>
-            <circle cx="12" cy="12" r="4"></circle>
-            <line x1="21.17" y1="8" x2="12" y2="8"></line>
-            <line x1="3.95" y1="6.06" x2="8.54" y2="14"></line>
-            <line x1="10.88" y1="21.94" x2="15.46" y2="14"></line>
-          </svg>
+          <svg class="w-6 h-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><circle cx="12" cy="12" r="4"></circle><line x1="21.17" y1="8" x2="12" y2="8"></line><line x1="3.95" y1="6.06" x2="8.54" y2="14"></line><line x1="10.88" y1="21.94" x2="15.46" y2="14"></line></svg>
         </a>
       </div>
-      <p class="mt-4 text-xs text-center text-gray-400">© 2023 Quizgecko, All
-        rights
-        reserved
+      <p class="mt-4 text-xs text-center text-gray-400">
+        © 2023 GreetingBirds, All rights reserved
       </p>
     </div>
   </footer>
@@ -75,13 +36,13 @@
 
 <script>
 import siteMetaInfo from "@/data/sitemetainfo";
-import BuyMeACoffee from "./BuyMeACoffee.vue";
+
 export default {
   data() {
     return {
       siteMetadata: siteMetaInfo,
     };
   },
-  components: { BuyMeACoffee },
+  components: { },
 };
 </script>
