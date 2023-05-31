2e078727aba4b36cdf473f353055abee85db81e2
erdi <erdi@digitexa.com>
2023-05-31 17:13:47 +0300

    auto merge check

--- data/menu.js
+++ data/menu.js
@@ -2,7 +2,6 @@ const menu = [
   { name: "Home", path: "/" },
   { name: "Tags", path: "/tags" },
   { name: "Blog", path: "/blog" },
-  { name: "Blogs", path: "/blog" },
 ];
 
 export default menu;
