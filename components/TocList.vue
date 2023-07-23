2c5a8f87abfc50a9b1e581251a1911890558c5dc
erdi <erdi@digitexa.com>
2023-07-23 17:53:31 +0300

    Added Toc and TocList SiteNavigationElement

--- components/TocList.vue
+++ components/TocList.vue
@@ -1,21 +1,23 @@
 <template>
-  <ul class="space-y-3">
-    <li v-for="item in items">
-      <a
-        class="font-semibold group-[.subheadings]:font-normal
-        hover:text-pink-700 hover:underline
-        underline-offset-4"
-        :href="`#${item.id}`">
-        {{ item.content }}
-      </a>
+  <nav itemscope itemtype="http://schema.org/SiteNavigationElement">
+    <ul class="space-y-3">
+      <li itemprop="hasPart" v-for="item in items">
+        <a itemprop="url"
+          class="font-semibold group-[.subheadings]:font-normal
+          hover:text-pink-700 hover:underline
+          underline-offset-4"
+          :href="`#${item.id}`">
+          <span itemprop="name">{{ item.content }}</span>
+        </a>
 
-      <TocList
-        class="mt-3 ml-5 group subheadings"
-        v-if="item.subheadings.length"
-        :items="item.subheadings"
-      />
-    </li>
-  </ul>
+        <TocList
+          class="mt-3 ml-5 group subheadings"
+          v-if="item.subheadings.length"
+          :items="item.subheadings"
+        />
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <script>
