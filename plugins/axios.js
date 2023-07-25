41e1656d5c0e6be26ff11e85ae96c685ef706cfc
erdi <erdi@digitexa.com>
2023-07-25 03:27:18 +0300

    .htacc

--- plugins/axios.js
+++ plugins/axios.js
@@ -3,6 +3,7 @@ import https from 'https';
 export default function ({$axios, store}) {
   $axios.defaults.headers.common = { Authorization: `Bearer ${process.env.PRIVATE_TOKEN}` };
   const agent = new https.Agent({
+
     rejectUnauthorized: false
   });
   $axios.onRequest(config => {
