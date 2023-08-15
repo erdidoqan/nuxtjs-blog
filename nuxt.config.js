4d7d350e9bb7991074bd05f7cb1bd22cdfbee66b
erdi <erdi@digitexa.com>
2023-08-15 15:32:05 +0300

    Amp Çalışmaları

--- nuxt.config.js
+++ nuxt.config.js
@@ -71,19 +71,21 @@ export default {
     "@nuxtjs/amp",
     "nuxt-compress"
   ],
+  amp: {
+    css: '~/assets/styles/amp-custom.scss',
+    origin: 'http://localhost:3000'
+  },
   hooks: {
-    // This hook is called before saving the html to flat file
-    'generate:page': (page) => {
+    /*'generate:page': (page) => {
       if (/^\/amp\//gi.test(page.route)) {
         page.html = ampify(page.html)
       }
     },
-    // This hook is called before serving the html to the browser
     'render:route': (url, page, { req, res }) => {
       if (/^\/amp\//gi.test(url)) {
         page.html = ampify(page.html)
       }
-    }
+    }*/
   },
   image: {
     domains: ['https://icerikplanla.com/img', 'https://icerikplanla.com']
