b21388d70c4e3aef71e89e0b4dc158c531a40208
erdi <erdi@digitexa.com>
2023-10-03 16:22:20 +0300

    Yeni sitemap kategori güncellemesi ver2

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,9 +1,14 @@
 <template>
-  <NuxtLayout>
-    <div class="errorSection">
-      <p>ERROR DETAILS: {{ error.statusCode }} {{ error.statusMessage }}</p>
-    </div>
-  </NuxtLayout>
+  <header class="pt-10">
+
+    <h1 class="mb-4 mt-5 text-4xl tracking-tight text-slate-800 font-extrabold">
+      No Posts Found
+    </h1>
+    <p class="max-w-3xl text-slate-700 dark:text-slate-400 p-5">
+      We didn't find the posts for that URL.
+    </p>
+
+  </header>
 </template>
 
 <script>
