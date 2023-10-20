c347c0aa51b55ab31025ea14ffdf27acb19cba77
erdi <erdi@digitexa.com>
2023-10-21 00:17:10 +0300

    Corporation, Organization ve WebSite JSONLD Güncellemesi

--- layouts/default.vue
+++ layouts/default.vue
@@ -23,25 +23,83 @@ export default {
   methods: {
     OrganizationJsonld() {
       return {
-        "@context": "https://schema.org",
-        "@type": "Organization",
+        "@type":[
+          "Corporation",
+          "Organization"
+        ],
+        "@id": process.env.PUBLISH_URL+"/#organization",
         "name": process.env.SITE_TITLE,
-        "url": 'https://' + process.env.PUBLISH_URL,
-        "logo": 'https://' + process.env.PUBLISH_URL + '/icons/icon.png',
-        "sameAs" : [
-          "http://www.facebook.com/your-profile",
-          "http://www.twitter.com/yourProfile",
-          "http://plus.google.com/your_profile"
-        ]
+        "url": process.env.PUBLISH_URL,
+        "sameAs":[
+          "https://www.facebook.com/greetingbirds",
+          "https://twitter.com/greetingbirds"
+        ],
+        "email":"rhett.oconner@walker.com",
+        "address":{
+          "@type":"PostalAddress",
+          "streetAddress":"4066 Schmeler Knoll, 05396",
+          "addressLocality":"New Neva",
+          "addressRegion":"California",
+          "postalCode":"05396",
+          "addressCountry":"USA"
+        },
+        "logo":{
+          "@type":"ImageObject",
+          "@id": process.env.PUBLISH_URL+"/#logo",
+          "url": process.env.PUBLISH_URL+"/icons/icon.png",
+          "contentUrl": process.env.PUBLISH_URL+"/icons/icon.png",
+          "caption": process.env.SITE_TITLE,
+          "inLanguage":"en",
+          "width":"512",
+          "height":"512"
+        },
+        "contactPoint":[
+          {
+            "@type":"ContactPoint",
+            "telephone":"824-766-2037",
+            "contactType":"customer support"
+          }
+        ],
+        "location":{
+          "@id": process.env.PUBLISH_URL+"/#place"
+        }
+      }
+    },
+    PlaceJsonld() {
+      return {
+        "@type":"Place",
+        "@id": process.env.PUBLISH_URL + "/#place",
+        "geo":{
+          "@type":"GeoCoordinates",
+          "latitude":"36.2659575",
+          "longitude":"-115.1301159"
+        },
+        "hasMap":"https://www.google.com/maps/search/?api=1&amp;query=36.2659575,-115.1301159",
+        "address":{
+          "@type":"PostalAddress",
+          "streetAddress":"4066 Schmeler Knoll, 05396",
+          "addressLocality":"New Neva",
+          "addressRegion":"California",
+          "postalCode":"05396",
+          "addressCountry":"USA"
+        }
       }
     },
     websiteJsonld() {
       return {
-        "@context": "https://schema.org",
-        "@type": "WebSite",
+        "@type":"WebSite",
+        "@id": process.env.PUBLISH_URL+"/#website",
+        "url": process.env.PUBLISH_URL,
         "name": process.env.SITE_TITLE,
-        "url": 'https://' + process.env.PUBLISH_URL,
-        "logo": 'https://' + process.env.PUBLISH_URL + '/icons/icon.png',
+        "publisher":{
+          "@id": process.env.PUBLISH_URL+"/#organization"
+        },
+        "inLanguage":"tr",
+        "potentialAction":{
+          "@type":"SearchAction",
+          "target": process.env.PUBLISH_URL+"/?s={search_term_string}",
+          "query-input":"required name=search_term_string"
+        }
       }
     },
   },
@@ -59,6 +117,9 @@ export default {
     },{
       type: 'application/ld+json',
       innerHTML: JSON.stringify(this.OrganizationJsonld())
+    },{
+      type: 'application/ld+json',
+      innerHTML: JSON.stringify(this.PlaceJsonld())
     }],
       __dangerouslyDisableSanitizers: ['script'],
     }
