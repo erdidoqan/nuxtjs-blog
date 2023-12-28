c800cdca2b35ffb6c189cd2caeb5705c2266d155
erdi <erdi@digitexa.com>
2023-12-28 16:56:20 +0300

    Detail meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ import fs from 'fs'
 const https = require('https')
 
 https.get(process.env.MAIN_URL || 'https://main.azbucuk.com'+'/atom/'+process.env.PUBLISH_URL, resp => resp.pipe(fs.createWriteStream('static/feed.xml')));
-console.log(https.get(process.env.MAIN_URL || 'https://main.azbucuk.com'+'/atom/'+process.env.PUBLISH_URL))
+console.log(process.env.MAIN_URL || 'https://main.azbucuk.com'+'/atom/'+process.env.PUBLISH_URL)
 https.get(process.env.MAIN_URL || 'https://main.azbucuk.com'+'/feed/'+process.env.PUBLISH_URL, resp => resp.pipe(fs.createWriteStream('static/rss.xml')));
 https.get(process.env.MAIN_URL || 'https://main.azbucuk.com'+'/sitemaps/image/'+process.env.PUBLISH_URL, resp => resp.pipe(fs.createWriteStream('static/sitemap-image.xml')));
 https.get(process.env.MAIN_URL || 'https://main.azbucuk.com'+'/sitemaps/news/'+process.env.PUBLISH_URL, resp => resp.pipe(fs.createWriteStream('static/news_sitemap.xml')));
