bb4f809f12cf0b3e287eb6d3ff38bd678fd538de
erdi <erdi@digitexa.com>
2024-01-02 15:53:22 +0300

    Breadcrumbs itemprop temizlemesi

--- components/Breadcrumbs.vue
+++ components/Breadcrumbs.vue
@@ -1,22 +1,17 @@
 <template>
-  <nav aria-label="breadcrumbs" class="flex items-center justify-center bg-grey-light rounded font-sans w-full">
+  <nav class="flex items-center justify-center bg-grey-light rounded font-sans w-full">
 
-    <ol itemscope itemtype="https://schema.org/BreadcrumbList" class="list-reset flex text-grey-dark">
+    <ol class="list-reset flex text-grey-dark">
 
       <template v-for="(list,key) in lists">
-        <li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem" :class="list.hidden ? 'hidden': ''" >
+        <li :class="list.hidden ? 'hidden': ''" >
 
           <NuxtLink
-            itemprop="item"
             :to="`${list.url + '/'}`"
-            data-vars-ga-ux-element="Breadcrumbs"
-            :data-vars-ga-call-to-action="list.name"
-            :data-vars-ga-outbound-link="list.url"
             class="text-gray-400 underline"
           >
-            <span itemprop="name">{{ list.name }}</span>
+            <span>{{ list.name }}</span>
           </NuxtLink>
-          <meta itemprop="position" :content="key + 1">
         </li>
         <li v-if="list.ok" ><span class="mx-2 breadcrumb-slash text-gray-400">/</span></li>
       </template>
