1306b0489543401c97a7650229a691d6ce0dad21
erdi <erdi@digitexa.com>
2024-08-21 15:08:19 +0300

    color settings

--- pages/index.vue
+++ pages/index.vue
@@ -151,7 +151,7 @@ export default {
     return {
       title: process.env.META_TITLE,
       desc: process.env.META_DESC,
-      mainColor: process.env.MAIN_COLOR,
+      mainColor: 'bg-yellow-800',
       md: markdownit(),
       dictionary: getDictionary(process.env.HTML_LANG),
     };
